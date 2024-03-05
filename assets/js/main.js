87aa651f4b9e54653b8922964a6c7ba511021445
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-05 16:03:25 +0100

    snack 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,27 @@ const animalList = [
   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' }
 ]
 
-const mammalsList = animalList.filter(mammals => mammals.classe === "mammiferi")
+const mammalsList = animalList.filter(mammals => mammals.classe === "mammiferi");
+
+console.log("la lista con i mammiferi è: ")
 console.log(mammalsList)
+
+const personList = [
+  {
+    nome: 'Carlo', cognome: 'Adile', età: 24
+  },
+  {
+    nome: 'Tizio', cognome: 'Caio', età: 22
+  },
+  {
+    nome: 'Pinco', cognome: 'Pallo', età: 17
+  }
+]
+
+const personCanDrink = personList.filter((checkPerson) => checkPerson.età >= 18)
+/* console.log(personCanDrink) */
+
+personCanDrink.forEach((person) => {
+  console.log(person.nome + ' ' + person.cognome + ' can drink')
+});
+
